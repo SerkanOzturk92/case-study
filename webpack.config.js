e63c766b6594185a2a89c7fa355397787824256d
Serkan Öztürk <serkan.ozturk@yildiztech.co>
2024-07-15 21:30:42 +0300

    add configs

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,30 @@
-const path = require("path");
+const path = require('path');
 
-module.exports={
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "public"),
-        filename: "main.js"
-    },
-    target: "web",
-    devServer: {
-        port: "3005",
-        static: ["./public"],
-        open: false,
-        hot: true ,
-        liveReload: true
-    },
-    resolve: {
-        extensions: ['.js','.jsx','.json']
-    },
-    module:{
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use:  'babel-loader'
-            }
-        ]
-    }
-}
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'main.js'
+  },
+  target: 'web',
+  devServer: {
+    port: '3005',
+    static: ['./public'],
+    open: false,
+    hot: true,
+    liveReload: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
